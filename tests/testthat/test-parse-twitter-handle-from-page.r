10472ec7fdff6f54784e89d41d0b5a9578869c5e
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-02-16 13:37:09 -0800

    add tesinng for issue #2

--- tests/testthat/test-parse-twitter-handle-from-page.r
+++ tests/testthat/test-parse-twitter-handle-from-page.r
@@ -8,3 +8,9 @@ test_that("MoveOn example check", {
     expect_equal(parse_twitter_handle_from_page("https://www.moveon.org"), "moveon")
     
 })
+
+test_that("Filtering rule for twitter links should be specific enough", {
+    
+    expect_equal(parse_twitter_handle_from_page("https://www.chshartford.org/"), "chshartford")
+    
+})
