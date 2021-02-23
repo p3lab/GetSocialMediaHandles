bdb0fad3cac4c563c9d1fe0a7532168892b82ad8
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2021-02-23 10:45:53 -0800

    add more stuff

--- tests/testthat/test-parse-twitter-handle-from-page.r
+++ tests/testthat/test-parse-twitter-handle-from-page.r
@@ -4,13 +4,9 @@ library(GetSocialMediaHandles)
 context("Parse Twitter handle from page")
 
 test_that("MoveOn example check", {
-    
-    expect_equal(parse_twitter_handle_from_page("https://www.moveon.org"), "moveon")
-    
+  expect_equal(parse_twitter_handle_from_page("https://www.moveon.org"), "moveon")
 })
 
 test_that("Filtering rule for twitter links should be specific enough", {
-    
-    expect_equal(parse_twitter_handle_from_page("https://www.chshartford.org/"), "chshartford")
-    
+  expect_equal(parse_twitter_handle_from_page("https://www.chshartford.org/"), "chshartford")
 })
